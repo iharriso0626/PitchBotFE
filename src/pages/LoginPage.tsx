5c2038e21908fef48634ade7aaa8e250293a6e39
iharriso0626 <your-email@example.com>
2024-09-11 13:53:34 -0500

    page update

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const LoginPage: React.FC = () => {
   return (
-    <div className="w-[1000px] h-[500px] absolute bg-white border-2 flex">
-        <h1 className='bg-white text-red-500'>Test Sillies</h1>
+    <div className='flex w-screen h-screen bg-black items-center justify-center'>
+        <div className='items-center justify-center'>Hi </div>
     </div>
   );
 };
